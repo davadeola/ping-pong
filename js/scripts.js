21e499948a2c29a9f39541f4abae414cffb45b14
David Adeola <adeoladavid15@gmail.com>
2017-06-09 11:11:36 +0300

    This will create the index.html and add assets

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var num = parseInt($("#num input").val());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $("#blanks form").submit(function(event) {
+      
+
+
+
+
+
+    event.preventDefault();
+  });
+
+
+
+});
